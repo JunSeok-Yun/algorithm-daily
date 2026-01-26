fc7c391498c3630e796bd9fae320eabdf13326cc
JunSeok-Yun <ujs9901@naver.com>
2026-01-26 17:23:20 +0900

    feat: BOJ 22479 solved

---
+++ boj/Silver/22479/Main.java
@@ -0,0 +1,62 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int N, M, R;
+	static int cnt = 0;
+	static boolean[] visited;
+	static int[] result;
+	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+
+		visited = new boolean[N + 1];
+		result = new int[N + 1];
+
+		for (int i = 0; i <= N; i++) {
+			graph.add(new ArrayList<>());
+		}
+
+		for (int i = 0; i< M; i++){
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+
+			graph.get(u).add(v);
+			graph.get(v).add(u);
+		}
+
+		for (int i = 0; i <= N; i++) {
+			Collections.sort(graph.get(i));
+		}
+
+		result[R] = 1;
+		visited[R] = true;
+		cnt = 2;
+		dfs(R);
+
+		for (int i = 1; i <= N; i++) {
+			sb.append(result[i]).append('\n');
+		}
+		System.out.println(sb);
+		br.close();
+	}
+
+	public static void dfs(int root) {
+		for (int vertex : graph.get(root)) {
+			if (!visited[vertex]) {
+				result[vertex] = cnt++;
+				visited[vertex] = true;
+				dfs(vertex);
+			}
+		}
+	}
+}
